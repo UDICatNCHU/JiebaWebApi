39375daab8bf082359bc9789bff2110c18c132ef
davidtnfsh <davidtnfsh@gmail.com>
2017-01-20 19:15:44 +0800

    finish part of speech

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 from api import views
 urlpatterns = [
 	url(r'^cut$', views.cut),
+	url(r'^pos$', views.pos),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import jieba
+import jieba.posseg as posseg
 # Create your views here.
 
 def cut(request):
 	if request.POST and request.method == 'POST':
 		data = request.POST
 		data = data.dict()
-		print(data)
 		return JsonResponse(jieba.lcut(data['sentence']), safe=False)
+	return JsonResponse({'result':False}, safe=False)
+
+def pos(request):
+	if request.POST and request.method == 'POST':
+		data = request.POST
+		data = data.dict()
+		return JsonResponse( list(map(lambda x:tuple(x), posseg.lcut(data['sentence']))), safe=False)
 	return JsonResponse({'result':False}, safe=False)
